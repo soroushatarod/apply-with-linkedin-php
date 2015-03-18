458e70eb46506afb50cf523301f0ebe233889778
Soroush Atarod <soroush.atarod@outlook.com>
2015-03-18 21:11:33 +0000

    fix the storage

--- linkedin.php
+++ linkedin.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: karkton
- * Date: 21/02/2015
- * Time: 19:17
- */
 
 require_once 'vendor/autoload.php';
 
@@ -15,15 +9,4 @@ $consumerSecret = '';
 
 $linkedin = new \Soroush\Linkedin\Linkedin($consumerKey, $consumerSecret);
 
-
-$people = $linkedin->getPeople()->asPdf();
-
-
-echo $people;
-die();
-
-//
-//
-//
-//$oauth->setToken($_REQUEST['oauth_token'], $tokenSecret);
-//$_SESSION['access_token'] = $oauth->getAccessToken($access_token_url);
+$linkedin->getApplication()->asPdf('pdfs/linkedin-application.pdf');
